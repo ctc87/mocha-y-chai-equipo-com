5ffc1f6a8beb5f7590ea951b57e15258e09aad5a
Oscar Catari Gutierrez <alu0100825893@ull.edu.es>
2016-03-10 09:54:58 +0000

    Clases separadas en ficheros

---
+++ js/medida.js
@@ -0,0 +1,74 @@
+"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+(function(exports) {
+
+//Clase Medida -constructor
+function Medida(valor,tipo) {
+  this.valor = valor;
+  this.tipo = "";
+  this.ca = XRegExp('(?<value> ^\\s*([-+]?\\d+(?:\\.\\d+)?(?:\\s*e\\s?[-+]?\\d+)?)\\s*?º?)\\s* # valor   \n\
+                    (?<units>    ((?:([Cc])(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?) |      # celsisu \n\
+                    (?:([Ff])(?:a(?:h(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?)?) |  # fahrenheit \n\
+                    (?:([Kk])(?:e(?:l(?:v(?:i(?:n)?)?)?)?)?))\\s*)                      # kelvin  \n\
+                    ((?<to> (?:\\s*to)?\\s*)                                             # to \n\
+                    (?<units2>    ((?:([Cc])(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?) |      # celsisu \n\
+                    (?:([Ff])(?:a(?:h(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?)?) |  # fahrenheit \n\
+                    (?:([Kk])(?:e(?:l(?:v(?:i(?:n)?)?)?)?)?))))?\\s*$                       # kelvin ', 'x');
+
+  if (tipo != undefined) {
+    console.log("hay tipo");
+    this.tipo = tipo;
+  }
+
+}
+
+//Calculo de la conversion de temperaturas
+Medida.prototype.calculate = function(original) {
+  var celsius = new Celsius();
+  var fahrenheit = new Fahrenheit();
+  var kelvin = new Kelvin();
+  var result = ["","","",""];
+  var m = new Array();
+  var temp = original.value;
+  var match = XRegExp.exec(temp, this.ca);
+
+  if (match) {
+    m[2] = match.units.trim()[0]; //unidadesr
+    m[1] = match.value.trim(); // valor
+    var regexp2 = /^([-+]?\d+(?:\.\d+)?)(?:e\s?)?([-+]?\d+)?$/
+    var numtemp = m[1].match(regexp2);
+    numtemp[1] = parseFloat(numtemp[1]);
+
+    if (undefined != numtemp[2]){
+      numtemp[2] = parseFloat(numtemp[2]);
+      var num = numtemp[1]*Math.pow(10, numtemp[2]);
+    }
+    else{
+      var num = parseFloat(numtemp[1]);
+    }
+    var type = m[2];
+    if (m[2] == 'c' || m[2] == 'C') {
+      result = celsius.calculate(m,num);
+    }
+    else {
+      if (m[2] == 'f' || m[2] == 'F') {
+        result = fahrenheit.calculate(m,num);
+      }
+      else {
+        result = kelvin.calculate(m,num);
+      }
+    }
+    if(match.units2) {
+      result[3] = match.units2;
+    }
+    return result;
+
+  } else {
+    return undefined;
+  }
+}
+
+exports.Medida = Medida;
+//Variable a usar en el main para comerzar la conversion
+exports.medida = new Medida("0c");
+
+})(this)
--- js/temperature.js
+++ js/temperature.js
@@ -1,76 +1,9 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 (function(exports) {
 
-//Clase Medida -constructor
-function Medida(valor,tipo) {
-  this.valor = valor;
-  this.tipo = "";
-  this.ca = XRegExp('(?<value> ^\\s*([-+]?\\d+(?:\\.\\d+)?(?:\\s*e\\s?[-+]?\\d+)?)\\s*?º?)\\s* # valor   \n\
-                    (?<units>    ((?:([Cc])(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?) |      # celsisu \n\
-                    (?:([Ff])(?:a(?:h(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?)?) |  # fahrenheit \n\
-                    (?:([Kk])(?:e(?:l(?:v(?:i(?:n)?)?)?)?)?))\\s*)                      # kelvin  \n\
-                    ((?<to> (?:\\s*to)?\\s*)                                             # to \n\
-                    (?<units2>    ((?:([Cc])(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?) |      # celsisu \n\
-                    (?:([Ff])(?:a(?:h(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?)?) |  # fahrenheit \n\
-                    (?:([Kk])(?:e(?:l(?:v(?:i(?:n)?)?)?)?)?))))?\\s*$                       # kelvin ', 'x');
-
-  if (tipo != undefined) {
-    console.log("hay tipo");
-    this.tipo = tipo;
-  }
-
-}
-
-//Calculo de la conversion de temperaturas
-Medida.prototype.calculate = function(original) {
-  var celsius = new Celsius();
-  var fahrenheit = new Fahrenheit();
-  var kelvin = new Kelvin();
-  var result = ["","","",""];
-  var m = new Array();
-  var temp = original.value;
-  var match = XRegExp.exec(temp, this.ca);
-
-  if (match) {
-    m[2] = match.units.trim()[0]; //unidadesr
-    m[1] = match.value.trim(); // valor
-    var regexp2 = /^([-+]?\d+(?:\.\d+)?)(?:e\s?)?([-+]?\d+)?$/
-    var numtemp = m[1].match(regexp2);
-    numtemp[1] = parseFloat(numtemp[1]);
-
-    if (undefined != numtemp[2]){
-      numtemp[2] = parseFloat(numtemp[2]);
-      var num = numtemp[1]*Math.pow(10, numtemp[2]);
-    }
-    else{
-      var num = parseFloat(numtemp[1]);
-    }
-    var type = m[2];
-    if (m[2] == 'c' || m[2] == 'C') {
-      result = celsius.calculate(m,num);
-    }
-    else {
-      if (m[2] == 'f' || m[2] == 'F') {
-        result = fahrenheit.calculate(m,num);
-      }
-      else {
-        result = kelvin.calculate(m,num);
-      }
-    }
-    if(match.units2) {
-      result[3] = match.units2;
-    }
-    return result;
-
-  } else {
-    return undefined;
-  }
-}
-
 //Clase Temperatura con herencia de Medida -constructor
 function Temperatura(valor,tipo) {
     Medida.call(this,valor,tipo);
-
 }
 
 Temperatura.prototype = Object.create(Medida.prototype);
@@ -79,7 +12,6 @@ Temperatura.prototype.constructor = Temperatura;
 //Clase Celsius con herencia de Temperatura -constructor
 function Celsius(valor,tipo) {
   Temperatura.call(this,valor,tipo);
-
 }
 
 Celsius.prototype = Object.create(Temperatura.prototype)
@@ -103,7 +35,6 @@ Celsius.prototype.calculate = function(m,num) {
 //Clase Fahrenheit con herencia de Temperatura -constructor
 function Fahrenheit(valor,tipo) {
   Temperatura.call(this,valor,tipo);
-
 }
 
 Fahrenheit.prototype = Object.create(Temperatura.prototype)
@@ -128,7 +59,6 @@ Fahrenheit.prototype.calculate = function(m,num) {
 //Clase Kelvin con herencia de Temperatura -constructor
 function Kelvin(valor,tipo) {
   Temperatura.call(this,valor,tipo);
-
 }
 
 Kelvin.prototype = Object.create(Temperatura.prototype)
@@ -149,6 +79,9 @@ Kelvin.prototype.calculate = function(m,num) {
   return result;
 }
 
-//Variable a usar en el main para comerzar la conversion
-exports.medida = new Medida("0c");
+exports.Temperatura = Temperatura;
+exports.Celsius = Celsius;
+exports.Fahrenheit = Fahrenheit;
+exports.Kelvin = Kelvin;
+
 })(this)
